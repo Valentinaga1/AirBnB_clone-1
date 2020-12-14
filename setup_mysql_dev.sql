9a703c795495fb688e97996eac28e1e3bada731d
Valentinaga1 <1881@holbertonschool.com>
2020-12-14 23:16:44 +0000

    prepares a MySQL server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- prepares a MySQL server for the project --
+CREATE DATABASE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON  performance_shcema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
