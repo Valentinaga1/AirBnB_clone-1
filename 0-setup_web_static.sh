231906fc1cf125244eac1642be0769dfb3807ca9
Valentinaga1 <1881@holbertonschool.com>
2020-12-22 16:36:53 -0500

    Bash to set up a web server for the deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,10 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -f -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
-sudo sed -i '20i\\tlocation /hbnb_static/{\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx start
