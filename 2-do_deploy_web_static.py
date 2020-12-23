7939ec58b5025415fb4cc909eba8cebe0e218570
Valentinaga1 <1881@holbertonschool.com>
2020-12-23 00:05:12 -0500

    tasks3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,6 @@
 """Fabric script that distributes an archive to your web servers, using the
 function do_deploy"""
 from fabric.api import put, run, env
-
 from os.path import exists
 
 env.hosts = ["35.185.80.75", "35.231.48.2"]
@@ -10,23 +9,24 @@ env.hosts = ["35.185.80.75", "35.231.48.2"]
 
 def do_deploy(archive_path):
     """Function to distributes an archive to your web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        file_name_without_ext = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}{}/".format(path, file_name_without_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path,
-                                               file_name_without_ext))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path,
-                                                    file_name_without_ext))
-        run("rm -rf {}{}/web_static".format(path, file_name_without_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/\
-             current".format(path, file_name_without_ext))
-        return True
-    except Exception:
+    if exists(archive_path) is True:
+        try:
+            file_name = archive_path.split("/")[-1]
+            file_name_without_ext = file_name.split(".")[0]
+            path = "/data/web_static/releases/"
+            put(archive_path, "/tmp/")
+            run("mkdir -p {}{}/".format(path, file_name_without_ext))
+            run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path,
+                                                   file_name_without_ext))
+            run("rm /tmp/{}".format(file_name))
+            run("mv {0}{1}/web_static/* {0}{1}/".format(path,
+                                                        file_name_without_ext))
+            run("rm -rf {}{}/web_static".format(path, file_name_without_ext))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {}{}/ /data/web_static/\
+            current".format(path, file_name_without_ext))
+            return True
+        except Exception:
+            return False
+    else:
         return False
