f794d8cbded104b2c1db0478d24bfad4efad63d5
Valentinaga1 <1881@holbertonschool.com>
2021-01-19 14:00:04 -0500

    improvments in the engine

--- models/state.py
+++ models/state.py
@@ -19,16 +19,10 @@ class State(BaseModel, Base):
         def cities(self):
             """getter attribute cities that returns the list of City instances
             with state_id equals to the current State.id"""
-            # me traigo todos los objetos del objects de storage que son
-            # ciudades
+
             objects = models.storage.all(City)
             list_of_city = []
             for city in objects.values():
-                # esto me va a devolver {class.id = {state_id:"parametros"},
-                # {state_ide:"parametross"}}. cuando llamamos all nos está
-                # retornando una lista de objetos, aún sin serializar, no es
-                # diccionario, accedemos a los objetos a través de un punto
                 if city.state_id == self.id:
-                    # si el state_id == al current id que es self.id
                     list_of_city.append(city)
             return list_of_city
