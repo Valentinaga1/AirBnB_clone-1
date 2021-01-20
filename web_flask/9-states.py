1106b8dfc90d39c8507d2c2d709f9eb715428dfd
Valentinaga1 <1881@holbertonschool.com>
2021-01-20 10:11:48 -0500

    Fixing id and not found conditions

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -22,18 +22,21 @@ def list_of_states():
         "9-states.html", states=states, condition="states_list")
 
 
-@app.route("/states<id>", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def list_of_states_id(id):
     """Function"""
-    states= storage.all(State)
-    state_id = states[id]
-    if state_id:
-        return render_template(
-            "9-states.html", state_id=state_id, condition="state_id")
-    else:
-        return render_template(
-            "9-states.html", condition="not_found")
-
+    states = storage.all(State)
+    # primero verificar que se pase el id
+    if id:
+        key = "State." + id
+        # luego verificar que key(State.421a5..) esté en el diccionario
+        if key in states:
+            state_id = states[key]
+            return render_template(
+                "9-states.html", state_id=state_id, condition="state_id")
+        else:
+            return render_template(
+                "9-states.html", condition="not_found")
 
 
 if __name__ == "__main__":
